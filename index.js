9ea113a5eb9e7069ac18d75553e8205742b6e23e
Шугаев Алексей Андреевич <a.shugaev@carprice.ru>
2019-01-19 15:14:36 +0300

    fix: delete old branch name width --amend

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const messageFile = process.env.HUSKY_GIT_PARAMS;
 const message = fs.readFileSync(messageFile, {
   encoding: 'utf-8',
 });
-const messageTitle = message.split('\n')[0];
+const messageTitle = message.split('\n')[0].replace(/(.+\s\|\s)?/, '');
+
 const branchName = require('child_process').execSync('git rev-parse --abbrev-ref HEAD', {
   encoding: 'utf-8',
 }).split('\n')[0];
