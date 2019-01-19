de4ab98126cfe00ff3630f962b221988f535aac1
Шугаев Алексей Андреевич <a.shugaev@carprice.ru>
2019-01-19 14:40:44 +0300

    feat: initial

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const messageFile = process.env.HUSKY_GIT_PARAMS;
+const message = fs.readFileSync(messageFile, {
+  encoding: 'utf-8',
+});
+const messageTitle = message.split('\n')[0];
+const branchName = require('child_process').execSync('git rev-parse --abbrev-ref HEAD', {
+  encoding: 'utf-8',
+}).split('\n')[0];
+
+const messageLines = message.split('\n');
+messageLines[0] = `${branchName} | ${messageTitle}`;
+fs.writeFileSync(messageFile, messageLines.join('\n'), {
+  encoding: 'utf-8',
+});
